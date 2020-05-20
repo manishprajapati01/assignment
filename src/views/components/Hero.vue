09cbc343e7c44342de77e6023c1f8e180b239eaa
Manish Prajapati <prajapatim@hcl.com>
2020-05-20 23:23:08 +0530

    First commit

---
+++ src/views/components/Hero.vue
@@ -0,0 +1,68 @@
+<template>
+    <section class="section-hero section-shaped my-0">
+        <div class="shape shape-style-1 garden"  style="background-image: url('img/garden.jpg');">
+           <!-- <span class="span-150"></span>
+            <span class="span-50"></span>
+            <span class="span-50"></span>
+            <span class="span-75"></span>
+            <span class="span-100"></span>
+            <span class="span-75"></span>
+            <span class="span-50"></span>
+            <span class="span-100"></span>
+            <span class="span-50"></span>
+            <span class="span-100"></span>-->
+        </div>
+        <div class="container shape-container d-flex align-items-center">
+            <div class="col px-0">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-lg-6  pt-lg">
+                        <h6 class="text-white mt-4 mb-5">WELCOME / SINGAPORE / CHANGI AIRPORT TO CITY CENTRE</h6>
+                        <h2 class="text-white mt-4 mb-5"><strong>Transfer Options from Singapore Changi Airport to City Centre</strong></h2>
+                        <div class="btn-wrapper">
+                            <base-button tag="a"
+                                         href="https://demos.creative-tim.com/vue-argon-design-system/documentation"
+                                         class="mb-3 mb-sm-0"
+                                         type="info"
+                                         icon="fa fa-code">
+                                Components
+                            </base-button>
+                            <base-button tag="a"
+                                         href="https://www.creative-tim.com/product/vue-argon-design-system"
+                                         class="mb-3 mb-sm-0"
+                                         type="white"
+                                         icon="ni ni-cloud-download-95">
+                                Download Vue
+                            </base-button>
+                        </div>
+                    </div>
+                </div>
+                <div class="row align-items-center justify-content-around stars-and-coded">
+                    <div class="col-sm-4">
+                        <span class="text-white alpha-7 ml-3">Star us on</span>
+                        <a href="https://github.com/creativetimofficial/argon-design-system" target="_blank" title="Support us on Github">
+                            <img src="img/brand/github-white-slim.png" style="height: 22px; margin-top: -3px">
+                        </a>
+                    </div>
+                    <div class="col-sm-4 mt-4 mt-sm-0 text-right">
+                        <span class="text-white alpha-7">Coded by</span>
+                        <a href="https://www.creative-tim.com" target="_blank" title="Creative Tim - Premium Bootstrap Themes and Templates">
+                            <img src="img/brand/creativetim-white-slim.png" class="ml-3" style="height: 30px;">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.garden {
+    margin-bottom: 50px;
+    background: no-repeat center center;
+    background-attachment: scroll;
+    position: relative;
+    background-size: cover;
+}
+</style>

7dc852709499ad654b7848eb34e462064c5f814d
userrashid <user.rashid@gmail.com>
2020-05-21 18:58:18 +0530

    - added a change for herder nav link

--- src/components/BaseNav.vue
+++ src/components/BaseNav.vue
@@ -21,7 +21,7 @@
 
             <slot name="container-after"></slot>
 
-            <div class="collapse navbar-collapse" :class="{show: toggled}" :id="contentId" v-click-outside="closeMenu">
+            <div class="collapse navbar-collapse justify-content-end" :class="{show: toggled}" :id="contentId" v-click-outside="closeMenu">
                 <div class="navbar-collapse-header">
                     <slot name="content-header" :close-menu="closeMenu"></slot>
                 </div>

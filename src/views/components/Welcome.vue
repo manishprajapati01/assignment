704618a3a571c43ca1e6dc48c70ad485b0dcf48d
Manish Prajapati <prajapatim@hcl.com>
2020-05-22 11:16:56 +0530

    more styling

--- src/views/components/Welcome.vue
+++ src/views/components/Welcome.vue
@@ -1,6 +1,22 @@
 <template>
   <section class="section section-components pb-0" id="section-components">
     <div class="container">
+      <div>
+        <b-card
+          title="Card Title"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Image"
+          img-top
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+        >
+          <b-card-text>Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text>
+
+          <b-button href="#" variant="primary">Go somewhere</b-button>
+        </b-card>
+      </div>
+
       <div class="row justify-content-center align-items-center">
         <div class="col-lg-7">
           <div class="card">
@@ -29,7 +45,42 @@
       </div>
 
       <div class="row justify-content-center align-items-center mt-3">
-        <div class="col-lg-8">Image box will be here</div>
+        <div class="col-lg-5">
+          Book a trusted, English-speaking driver to pick you up at the same price as a regular taxi
+          <a
+            href="#!"
+          >Learn more</a>
+        </div>
+        <div class="col-lg-5">
+          <form>
+            <div class="input-group">
+              <input
+                type="text"
+                class="form-control form-control-sm"
+                value="Singapore Changi Airport (SIN)"
+              />
+              <i class="search-clear fa fa-times"></i>
+            </div>
+            <div class="input-group mt-1">
+              <input type="text" class="form-control form-control-sm" value="Pan Pacific Singapore" />
+              <i class="search-clear fa fa-times"></i>
+            </div>
+            <div class="form-row">
+              <div class="form-group mt-1 col-md-6">
+                <date-pickers></date-pickers>
+              </div>
+              <div class="form-group mt-1 col-md-6">
+                <label for="inputAddress">Time</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="inputAddress"
+                  placeholder="1234 Main St"
+                />
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
 
       <div class="row justify-content-center align-items-center mt-3">
@@ -112,8 +163,65 @@
         </div>
       </div>
 
-      <div class="row justify-content-center align-items-center mt-3">
-        <div class="col-lg-7">Rating box will be here</div>
+      <div class="row justify-content-center align-items-center mr-1 ml-1 mt-3">
+        <div class="col-lg-4 border-top border-right border-left">
+          <div class="row mt-4">
+            <div class="col-sm-6 col-lg-6">
+              <div class="text-uppercase">Cost</div>
+              <h3>
+                <i class="fa fa-eur" aria-hidden="true"></i>1.20
+              </h3>
+            </div>
+            <div class="col-sm-6 col-lg-6">
+              <div class="text-uppercase">Trip duration</div>
+              <h3>30 Min.</h3>
+            </div>
+          </div>
+          <div class="row mb-2">
+            <div class="col-lg-12">There can be up to a 13 mintue wait for the metro</div>
+          </div>
+        </div>
+        <div class="col-lg-3 border-top border-right">
+          <div class="row mt-3">
+            <div class="col-sm-6 col-lg-6 text-uppercase">Convenience</div>
+            <div class="col-sm-6 col-lg-6 text-uppercase">
+              <span class="fa fa-star checked ml-1"></span>
+              <span class="fa fa-star checked ml-1"></span>
+              <span class="fa fa-star checked ml-1"></span>
+              <span class="fa fa-star ml-1"></span>
+              <span class="fa fa-star ml-1"></span>
+            </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col-sm-6 col-lg-6 text-uppercase">Safety</div>
+            <div class="col-sm-6 col-lg-6 text-uppercase">
+              <span class="fa fa-star checked ml-1"></span>
+              <span class="fa fa-star checked ml-1"></span>
+              <span class="fa fa-star checked ml-1"></span>
+              <span class="fa fa-star ml-1"></span>
+              <span class="fa fa-star ml-1"></span>
+            </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col-sm-6 col-lg-6 text-uppercase">Value for money</div>
+            <div class="col-sm-6 col-lg-6 text-uppercase">
+              <span class="fa fa-star checked ml-1"></span>
+              <span class="fa fa-star checked ml-1"></span>
+              <span class="fa fa-star checked ml-1"></span>
+              <span class="fa fa-star ml-1"></span>
+              <span class="fa fa-star ml-1"></span>
+            </div>
+          </div>
+          <div class="row mt-4"></div>
+        </div>
+      </div>
+      <div class="row justify-content-center align-items-center mr-1 ml-1">
+        <div class="col-lg-7 border">
+          <p class="mt-2">
+            <i class="fa fa-info-circle" aria-hidden="true"></i>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cras adipiscing enim eu turpis egestas pretium.
+          </p>
+        </div>
       </div>
 
       <div class="row justify-content-center align-items-center mt-4">
@@ -164,6 +272,7 @@
             <div class="card">
               <div class="card-header" id="headingOne">
                 <h5 class="mb-0">EDITOR'S NOTE</h5>
+                <i class="fa fa-chevron-up" aria-hidden="true"></i>
               </div>
 
               <div id="collapseOne" class="collapse show">
@@ -176,6 +285,7 @@
             <div class="card mt-3">
               <div class="card-header" id="headingOne">
                 <h5 class="mb-0">USEFUL TIPS</h5>
+                <i class="fa fa-chevron-down" aria-hidden="true"></i>
               </div>
 
               <div id="collapseOne" class="collapse">
@@ -185,13 +295,62 @@
               </div>
             </div>
           </div>
+
+          <div class="mb-5 d-flex">
+            <div class="flex-grow-1">
+              <img src="img/images.jpg" alt />
+            </div>
+            <div class="athen-container">
+              <div class="pt-3 pl-5 pr-3">
+                <h6>Welcome</h6>
+                <h4>The best way to arrive in Athens</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <button class="btn btn-success">Learn more</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center align-items-center mt-4 mb-4">
+        <div class="col-lg-5">
+          <p
+            class="mt-2 mb-2 text-muted font-weight-bold text-uppercase small"
+          >Save the article for later or share it with friends</p>
+          <div class="share-article">
+            <a href="#!" class="mr-4 ml-1">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+            </a>
+            <a href="#!" class="mr-4">
+              <i class="fa fa-whatsapp" aria-hidden="true"></i>
+            </a>
+            <a href="#!" class="mr-4">
+              <i class="fa fa-weixin" aria-hidden="true"></i>
+            </a>
+            <a href="#!" class="mr-4">
+              <i class="fa fa-facebook-official" aria-hidden="true"></i>
+            </a>
+            <a href="#!" class="mr-4">
+              <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+            <button class="btn btn-sm btn-outline-dark">Copy Link</button>
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
 <script>
-export default {};
+const DatePickers = () => import("./JavascriptComponents/DatePickers");
+import { BCard, BCardText, BButton } from "bootstrap-vue";
+
+export default {
+  components: {
+    DatePickers,
+    BCard,
+    BCardText,
+    BButton
+  }
+};
 </script>
 <style>
 .card-horizontal {
@@ -200,5 +359,29 @@ export default {};
 }
 .checked {
   color: #71eeb8;
+}
+.share-article i {
+  color: #c7c7c7;
+  font-size: 1.5em;
+}
+.athen-container {
+  background-color: rgba(0, 0, 0, 0.03);
+}
+.form-control-clear {
+  z-index: 10;
+  pointer-events: auto;
+  cursor: pointer;
+}
+
+.search-clear {
+  position: absolute;
+  right: 5px;
+  top: 0;
+  bottom: 0;
+  height: 14px;
+  margin: auto;
+  font-size: 14px;
+  cursor: pointer;
+  color: #ccc;
 }
 </style>
